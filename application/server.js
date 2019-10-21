af9f8357450c3b0b73c852f3ab7e9daa1bf1013e
Plora Tran <ploratran@gmail.com>
2019-10-21 16:01:36 -0700

    update: milestone 2: UI

---
+++ application/server.js
@@ -0,0 +1,39 @@
+const express = require('express'); //express framework 
+const path = require('path');
+const mysql = require('mysql');
+const bodyparser = require('body-parser');
+const aboutRouter = require('./routers/about');
+const homeRouter = require('./routers/home');
+const port = 3000; //port #, can change if there is an issue persisting
+
+const app = express();
+app.use(bodyparser.json());
+
+// const mysqlConnection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'testtest',
+//     database: 'testdb'
+// });
+
+// mysqlConnection.connect((err) => {
+//     if (!err) {
+//         console.log('Database successfully connected');
+//     } else {
+//         console.log('Error connecting: ' + JSON.stringify(err, undefined, 2));
+//     }
+// });
+
+app.set('view engine', 'ejs'); //set view engine as ejs
+app.set('views', path.join(__dirname, 'views')); //serve files in views folder
+
+app.use(express.static('public')); //serve public static files
+
+app.use('/', homeRouter);
+app.use('/', aboutRouter);
+
+app.use(function(req,res) {
+    res.status(400).render(path.join(__dirname, '/views/pages/404'));
+});
+
+app.listen(port, () => console.log('Listening on port 3000'));
