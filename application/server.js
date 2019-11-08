611b7f8a8158f60ef0b21f83f45571c0c500c9b3
your name <your email>
2019-11-07 23:12:06 -0800

    Update: New branch with file upload working

--- application/server.js
+++ application/server.js
@@ -1,7 +1,72 @@
-const express = require('express'); //express framework 
+const express = require('express'); //express framework
+const multer = require('multer');
+const ejs = require('ejs');
+
+
 var path = require('path');
-var router = express.Router(); //router object via express 
+var router = express.Router(); //router object via express
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+        checkFileType(file, cb);
+    }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname){
+        return cb(null,true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
+
 const app = express();
+
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'))
+app.get('/', (req, res) => res.render('index'));
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            if(req.file == undefined){
+                res.render('index', {
+                    msg: 'Error: No File Selected!'
+                });
+            } else {
+                res.render('index', {
+                    msg: 'File Uploaded!',
+                    file: `uploads/${req.file.filename}`
+                });
+            }
+        }
+    });
+});
+
 app.use(express.static(path.join(__dirname+'/about'))); //serves about.css static
 app.use("/about", express.static((__dirname+'/public'))); //this is supposed to serve the css, but does not
 router.get('/', function (req, res){
