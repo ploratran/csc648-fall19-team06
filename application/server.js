4906d672c70bd396170f75c6b95969dea9d237c8
960399552 <35763873+960399552@users.noreply.github.com>
2019-11-26 12:55:30 -0800

    js file

--- application/server.js
+++ application/server.js
@@ -1,29 +1,33 @@
 const express = require('express'); //express framework 
+const fileUpload = require('express-fileupload'); //file upload
 const path = require('path');
 const mysql = require('mysql');
 const bodyparser = require('body-parser');
-const db = require('./model/database');
-const port = 3000; //port #, can change if there is an issue persisting
-
-// connect to db:
-db.connect((err) => {
-    if (err) {
-        console.log('Error connecting MySQL Database...');
-        return;
-    }
-    console.log('MySQL Database Connected...');
-})
-
 // initialize app:
 const app = express();
+const db = require('./model/database');
+const port = 3000; //port #, can change if there is an issue persisting
+const pages = path.join(__dirname, '/views/pages');
 
-// define all routes: 
+
+// connect to db:
+//db.connect((err) => {
+//    if (err) {
+//        console.log('Error connecting MySQL Database...');
+//        return;
+//    }
+//    console.log('MySQL Database Connected...');
+//})
+
+//Global declare variables
+global.pages = pages;
+//global.db = db; //globally declares db
+
+const {getHomePage, sell, login, register, forgotPassword,about, searchCategory} = require('./routers/home');
+const {searchProducts, addProductPage, addProduct} = require('./routers/search');
+// const {about, aboutTT} = require('./routers/about');
 const aboutRouter = require('./routers/about');
-const homeRouter = require('./routers/home');
-const sellRouter = require('./routers/sell');
-const loginRouter = require('./routers/login');
-const registerRouter = require('./routers/register');
-const forgotRouter = require('./routers/forgot-password');
+app.use('/', aboutRouter);
 
 // set view engine as ejs:
 app.set('view engine', 'ejs'); 
@@ -31,14 +35,20 @@ app.set('views', path.join(__dirname, 'views')); //serve files in views folder
 
 // all middlewares: 
 app.use(bodyparser.json());
+app.use(fileUpload()); // configure fileupload
 app.use(express.static('public')); //serve public static files
 
-app.use('/', homeRouter);
-app.use('/', aboutRouter);
-app.use('/', sellRouter);
-app.use('/', loginRouter);
-app.use('/', registerRouter);
-app.use('/', forgotRouter);
+
+app.get('/', getHomePage);
+app.get('/sell', sell);
+app.get('/login', login);
+app.get('/register', register);
+app.get('/forgot-password', forgotPassword);
+app.post('/searchProducts', searchProducts);
+app.get('/searchCategory/:category', searchCategory);
+app.get('/addProduct', addProductPage);
+app.post('/addProduct', addProduct);
+app.get('/about', about);
 
 app.use(function(req,res) {
     res.status(400).render(path.join(__dirname, '/views/pages/404'));
